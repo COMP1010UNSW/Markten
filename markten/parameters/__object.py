4616c6d84159688a25471a0b0bbc4056f941877d
Maddy Guthridge <hello@maddyguthridge.com>
2025-03-02 16:55:30 +1100

    Fix from_object param generator string handling
    Resolves #5

--- markten/parameters/__object.py
+++ markten/parameters/__object.py
@@ -6,18 +6,33 @@ def from_object(
     obj: object,
     param_names: Sequence[str],
 ) -> dict[str, Sequence[Any]]:
-    """
-    Get params from an object.
+    """Generate a mapping of parameters from an object.
 
-    This can be used with argparse results.
+    This is most useful when used with argparse namespaces.
+
+    If a value is a base data type, it will automatically be converted to a
+    single-element tuple. Note that although `str` is a `Sequence[str]`,
+    strings will not be separated into characters.
+
+    Parameters
+    ----------
+    obj : object
+        Object to gather parameters from.
+    param_names : Sequence[str]
+        Names of parameters to gather.
+
+    Returns
+    -------
+    dict[str, Sequence[Any]]
+        Mapping of parameters.
     """
     params = {}
 
     for name in param_names:
         value = getattr(obj, name)
-        if isinstance(value, Sequence):
+        if isinstance(value, Sequence) and not isinstance(value, str):
             params[name] = value
         else:
-            params[name] = [value]
+            params[name] = (value,)
 
     return params
