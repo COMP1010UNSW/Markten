048cbc9923f3f27c2042c401887044f2ed0aa427
Maddy Guthridge <hello@maddyguthridge.com>
2025-07-11 23:49:52 +1000

    Change action type to new format

--- markten/actions/__action.py
+++ markten/actions/__action.py
@@ -1,53 +1,76 @@
-"""
-# MarkTen / actions / action
+from collections.abc import AsyncGenerator, Callable
+from typing import Any
 
-Base class for MarkTen actions.
-"""
-
-from abc import abstractmethod
-from typing import Any, Protocol, runtime_checkable
-
+from markten.__recipe.step import run_to_completion, start_generator
 from markten.__spinners import SpinnerTask
 
+ActionResult = Any | dict[str, Any]
+"""
+Result from a Markten action.
 
-@runtime_checkable
-class MarkTenAction(Protocol):
+Either a single value or a dict mapping from parameter names to their
+corresponding values.
+
+* Single values with no name will be discarded if used directly as a step.
+* Dict values will be added to the `context` for future steps.
+"""
+
+ActionGenerator = AsyncGenerator[ActionResult, None]
+"""
+A partially-executed action -- essentially a generator function which will
+yield a new state.
+"""
+
+MarktenAction = Callable[..., AsyncGenerator[ActionResult, None]]
+"""
+A Markten action is an async generator function which optionally yields a state
+to be used in future steps.
+
+It is called, with the `anext` function being used to execute the action. Once
+the function evaluates, it should yield a new state. Any required clean-up
+should be written after this `yield`. The generator should only `yield` one
+value. All other values will be ignored.
+"""
+
+
+def dict_to_actions(action: dict[str, MarktenAction]) -> list[MarktenAction]:
+    """Convert the given dictionary of actions into a list of actions.
+
+    All the given actions will be run in parallel.
+
+    Parameters
+    ----------
+    action : dict[str, MarktenAction]
+        Action dictionary
+
+    Returns
+    -------
+    list[MarktenAction]
+        Each action in the dictionary as its own independent action.
     """
-    An action object, which executes the given action.
+    result = []
+    for name, fn in action.items():
 
-    These objects are used by MarkTen to handle running a task, and performing
-    any required cleanup afterwards.
-    """
+        def make_generator(name, fn):
+            """
+            Make the generator function.
 
-    @abstractmethod
-    def get_name(self) -> str:
-        """
-        Returns the name to use for the action.
-        """
+            Needed to capture the `name` and `fn` loop variables, else they
+            will end up being the last value of the iteration.
 
-    @abstractmethod
-    async def run(self, task: SpinnerTask) -> Any:
-        """
-        Run the action.
+            https://docs.astral.sh/ruff/rules/function-uses-loop-variable/
+            """
 
-        This should perform setup for the action. Its resultant awaitable
-        should resolve once the setup is complete.
+            async def generator(
+                task: SpinnerTask, **kwargs
+            ) -> ActionGenerator:
+                """The actual generator function"""
+                gen = start_generator(fn, kwargs, task)
+                yield {name: await anext(gen)}
+                await run_to_completion(gen)
 
-        It can also use the `task` object to display progress for the task and
-        log any required output.
+            return generator
 
-        The awaited result may be used as a parameter for future steps. For
-        example, the `git.clone` action gives the path to the temporary
-        directory cloned.
-        """
-        raise NotImplementedError
+        result.append(make_generator(name, fn))
 
-    @abstractmethod
-    async def cleanup(self) -> None:
-        """
-        Clean up after the recipe has been run, performing any required
-        tear-down.
-
-        The resultant awaitable should resolve once the tear-down is complete.
-        """
-        raise NotImplementedError
+    return result
