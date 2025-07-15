b5351505f7935745dc55e3f615777752909ffbfe
Maddy Guthridge <hello@maddyguthridge.com>
2025-07-15 20:39:27 +1000

    Remove Python actions since they can be easily recreated with the new design

--- markten/actions/__init__.py
+++ markten/actions/__init__.py
@@ -3,7 +3,7 @@
 
 Code defining actions that are run during the marking recipe.
 """
-from . import editor, git, process, python, time, webbrowser
+from . import editor, git, process, time, webbrowser
 from .__action import MarktenAction
 
 __all__ = [
@@ -11,7 +11,6 @@ __all__ = [
     'editor',
     'git',
     'process',
-    'python',
     'time',
     'webbrowser',
 ]
