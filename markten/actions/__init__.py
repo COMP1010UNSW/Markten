49f33e9de856988745590656288b30f84316419f
Maddy Guthridge <hello@maddyguthridge.com>
2025-07-19 23:58:49 +1000

    Add open action

--- markten/actions/__init__.py
+++ markten/actions/__init__.py
@@ -6,11 +6,13 @@ Code defining actions that are run during the marking recipe.
 
 from . import editor, git, process, time, webbrowser
 from .__action import MarktenAction
+from .__misc import open
 
 __all__ = [
     "MarktenAction",
     "editor",
     "git",
+    "open",
     "process",
     "time",
     "webbrowser",
